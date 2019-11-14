eaa8bae90999a012e332c422a51b2a84b735e1bb
Christopher Cowden <chris.cowden@gmail.com>
2019-11-13 19:43:02 -0600

    reorganizing and introducing a setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup
+
+setup(
+  name='smeagol',
+  version='0.0',
+  packages='smeagol',
+  author='Christopher Cowden',
+  author_email='chris.cowden@gmail.com',
+  description='A ring finder.  Applying convolutional filters to engineer advanced features for interpretable models.',
+  url='https://www.github.com/cowden/smeagol'
+)
